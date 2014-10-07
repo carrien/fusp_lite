98c9123a1d075d9de3880272773d3a7c62568993
Carrie Niziolek <carrien@ucsf>
2014-10-07 15:02:47 -0700

    Code cleanup.

--- fusp_lite_code/get_port_number.m
+++ fusp_lite_code/get_port_number.m
@@ -4,17 +4,19 @@ function [port_number] = get_port_number(basedir)
 if nargin < 1, basedir = cd; end
 
 base_port_number = 51717;
-fpportn = fopen([basedir '/last_port_used.txt'],'r');
+portfilename = 'last_port_used.txt';
+portfile = fullfile(basedir,portfilename);
+fpportn = fopen(portfile,'r');
 if fpportn == -1
-  fpportn = fopen([basedir '/last_port_used.txt'],'w');
+  fpportn = fopen(portfile,'w');
   if fpportn == -1
-    error('cannot open last_port_used.txt file');
+    error('cannot open %s file',portfilename);
   end
   fprintf(fpportn,'%d\n',base_port_number);
   fclose(fpportn);
-  fpportn = fopen([basedir '/last_port_used.txt'],'r');
+  fpportn = fopen(portfile,'r');
   if fpportn == -1
-    error('cannot find last_port_used.txt file');
+    error('cannot find %s file',portfilename);
   end
 end
 last_port_number = fscanf(fpportn,'%d');
@@ -23,7 +25,7 @@ port_number = last_port_number + 1;
 if port_number > base_port_number + 5
   port_number = base_port_number;
 end
-fpportn = fopen([basedir '/last_port_used.txt'],'w');
+fpportn = fopen(portfile,'w');
 fprintf(fpportn,'%d\n',port_number);
 fclose(fpportn);
 
--- fusp_lite_code/startup_fusp_lite.m
+++ fusp_lite_code/startup_fusp_lite.m
@@ -1,5 +1,7 @@
-function fusp_fd = startup_fusp_lite(p)
+function ffd = startup_fusp_lite(p)
 %STARTUP_FUSP_LITE  Opens TCPIP port with fusp server.
+%   STARTUP_FUSP_LITE(P) opens a TCP/IP port given the parameters in P,
+%   returning the fusp file descriptor FFD.
 
 client_bufsize = 8192;
 if rem(client_bufsize,4)
@@ -7,13 +9,13 @@ if rem(client_bufsize,4)
 end
 
 if p.fusp_init.port_number == -1
-    fusp_fd = -1;
+    ffd = -1;
 else
-    fusp_fd = tcpip(p.fusp_ip,p.fusp_init.port_number);
-    set(fusp_fd,'InputBufferSize',client_bufsize);
-    set(fusp_fd,'OutputBufferSize',client_bufsize);
-    fopen(fusp_fd);
-    set(fusp_fd,'ByteOrder','LittleEndian');
+    ffd = tcpip(p.fusp_ip,p.fusp_init.port_number);
+    set(ffd,'InputBufferSize',client_bufsize);
+    set(ffd,'OutputBufferSize',client_bufsize);
+    fopen(ffd);
+    set(ffd,'ByteOrder','LittleEndian');
 end
-send_command_to_fusp(fusp_fd,'Fusp server started');
-fusp_new_cpset(fusp_fd,sprintf('client_bufsize(%d)',client_bufsize));
+send_command_to_fusp(ffd,'Fusp server started');
+fusp_new_cpset(ffd,sprintf('client_bufsize(%d)',client_bufsize));
