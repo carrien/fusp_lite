ff34464bf2b7326fb6f5f74481b797951d022c98
Carrie Niziolek <carrien@ucsf>
2014-10-07 15:03:21 -0700

    Bugfix to handle null case (empty input struct).

--- fusp_lite_code/extend_struct.m
+++ fusp_lite_code/extend_struct.m
@@ -1,6 +1,8 @@
 function [strct] = extend_struct(strct,newstrct)
 
-newfields = fieldnames(newstrct);
-for i=1:length(newfields)
-    strct.(newfields{i}) = newstrct.(newfields{i});
+if ~isempty(newstrct)
+    newfields = fieldnames(newstrct);
+    for i=1:length(newfields)
+        strct.(newfields{i}) = newstrct.(newfields{i});
+    end
 end
